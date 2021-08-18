aca9f90daedc5a9cac4f4adc556aa46494ecbfd7
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-08-18 18:26:12 +0900

    Decorator Pattern

---
+++ src/decorator/Main.java
@@ -0,0 +1,39 @@
+package decorator;
+
+import decorator.abst.IBeverage;
+import decorator.concrete.Base;
+import decorator.concrete.Espresso;
+import decorator.concrete.Milk;
+
+import java.util.Scanner;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		// base
+		IBeverage beverage = new Base();
+		boolean done = false;
+		while (!done) {
+			System.out.println("음료 현재 가격 : "+beverage.getTotalPrice());
+			System.out.print("선택 : 1:샷 추가 / 2:우유 추가");
+			switch (sc.nextInt()) {
+			case 0: done = true;
+				break;
+			case 1: 
+				beverage = new Espresso(beverage);
+				break;
+			case 2:
+				beverage = new Milk(beverage);
+				break;
+			}
+		}
+
+		System.out.println("음료 가격 : "+beverage.getTotalPrice());
+		sc.close();
+	}
+
+}
